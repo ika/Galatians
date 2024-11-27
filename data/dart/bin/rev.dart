06f425a51fa060d92834e6c881478cc03528a09d
Ian K Armstrong <ian.k.armstrong@gmail.com>
2024-11-27 16:29:39 +0200

    update

--- data/dart/bin/rev.dart
+++ data/dart/bin/rev.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 import 'package:sqlite3/sqlite3.dart';
 
-String filePath = './rev.txt';
-String dbPath = './rev.db';
-String tableName = 'rev';
+String filePath = './galatians.txt';
+String dbPath = './gal.db';
+String tableName = 'galatians';
 
 void main() async {
   if (!await File(dbPath).exists()) {
@@ -30,9 +30,9 @@ void main() async {
         for (var line in lines) {
           // remove spaces
           var out = line.replaceAll(RegExp(r"\s+"), " ");
-          stmt.execute([out]);
+          //stmt.execute([out]);
           // or
-          //print(out);
+          print(out);
         }
       }
       print('COMPLETE');
