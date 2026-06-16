a497b437cbd65d5d16871409ce27f4e9b31c5f93
Ian K Armstrong <ian.k.armstrong@gmail.com>
2026-06-16 12:02:14 +0200

    first commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,67 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "org.armstrong.ika.galatians"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+//    kotlinOptions {
+//        jvmTarget(21)
+//    }
+
+    kotlin {
+        jvmToolchain(21)
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "org.armstrong.ika.galatians"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
+            storeFile = if (keystoreProperties.containsKey("storeFile")) rootProject.file(keystoreProperties["storeFile"] as String) else null
+            storePassword = keystoreProperties["storePassword"] as? String
+        }
+    }
+
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+//-------------------------------------------------------------
+// Constants
+//-------------------------------------------------------------
+
+class Constants {
+
+  static const String mainBaseName = "main.db";
+  static const String mainTableName = "gal";
+
+  static const String cacheBaseName = "cache.db";
+  static const String cacheTableName = "cache";
+
+}
